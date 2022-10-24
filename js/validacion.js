dc6749e61fc4591d598963cfbfcc10523805cee2
mika2128 <90878800+mika2128@users.noreply.github.com>
2022-10-24 16:09:55 -0300

    validacion
    cambio de msj

--- js/validacion.js
+++ js/validacion.js
@@ -26,7 +26,7 @@ formulario.addEventListener("submit", evento => {
     }
 
     if (texto.value.length < 10) {
-        alertas += `<b><p style="color:#FF5733">Se debe completar el campo con 10 caracteres o más</b></p>`;
+        alertas += `<b><p style="color:#FF5733">Se debe completar el campo "mensaje" con 10 caracteres o más</b></p>`;
         correcto = false;
     }
 
